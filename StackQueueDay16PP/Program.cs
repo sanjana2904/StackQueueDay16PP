e705facbfc50670a441e9c8e643f8140e5ad4cd8
sanjana k <sanjanarthi@gmail.com>
2021-03-24 22:09:31 +0530

    Add project files.

---
+++ StackQueueDay16PP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackQueueDay16PP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack Queue Practice Problem");
+        }
+    }
+}
